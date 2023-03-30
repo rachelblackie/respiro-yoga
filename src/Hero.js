cbb7250e369efd8c0f02ac5aabc879bd410f577b
rachelblackie <rachelannblackie@gmail.com>
2023-03-30 09:48:42 +0200

    adjusting CSS for mobile

--- src/Hero.js
+++ src/Hero.js
@@ -1,17 +1,21 @@
 import React from "react";
 
 import "./Hero.css";
-import fireBackground from "./fireBackground.jpg";
+import fireBackgroundMobile from "./fireBackgroundMobile.jpg";
 
 export default function Hero() {
   return (
     <div className="head-text">
       <div className="head-image position-relative">
-        <img src={fireBackground} className="img-fluid" alt="flames"></img>
+        <img
+          src={fireBackgroundMobile}
+          className="img-fluid"
+          alt="flames"
+        ></img>
       </div>
 
       <div className="text-on-image text-center">
-        <h1>Respiro Yoga</h1>
+        <h1 className="hero-heading">Respiro Yoga</h1>
         <h4>Lorem ipsum dolor sit amet</h4>
       </div>
     </div>
