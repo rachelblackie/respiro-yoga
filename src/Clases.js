319d40d0c868a7c0f924a0c9d9cd3802801ba4b9
rachelblackie <rachelannblackie@gmail.com>
2023-03-30 10:57:12 +0200

    CSS changes for mobile

--- src/Clases.js
+++ src/Clases.js
@@ -1,16 +1,41 @@
 import React from "react";
 import meditation from "./meditation.png";
 import "./App.css";
+import "./Clases.css";
 
 export default function Clases() {
   return (
     <div>
       <h2 className="text-center m-3">Clases</h2>
       <div className="row">
+        <div className="tipo-de-clase m-3">
+          <ul>
+            <li>Tipo de clase</li>
+            <ul>
+              <li>más información</li>
+            </ul>
+            <li>Tipo de clase</li>
+            <ul>
+              <li>más información</li>
+            </ul>
+            <li>Tipo de clase</li>
+            <ul>
+              <li>más información</li>
+            </ul>
+            <li>Tipo de clase</li>
+            <ul>
+              <li>más información</li>
+            </ul>
+            <li>Tipo de clase</li>
+            <ul>
+              <li>más información</li>
+            </ul>
+          </ul>
+        </div>
         <div className="col text-center my-auto">
           <img
             src={meditation}
-            className="img-fluid"
+            className="img-fluid clases-image"
             alt="illustration of woman meditating"
           ></img>
         </div>
--- src/Menu.js
+++ src/Menu.js
@@ -8,26 +8,35 @@ import "./Menu.css";
 export default function Menu() {
   return (
     <div>
-      <Navbar bg="light" variant="light" className="fixed-top">
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="light"
+        variant="light"
+        className="fixed-top"
+      >
         <Container>
           <Navbar.Brand href="#" className="navbar-left">
             <i class="fa-sharp fa-solid fa-fire-flame-curved"></i> Respiro Yoga
           </Navbar.Brand>
-          <Nav className="mx-auto Nav nav-links">
-            <Nav.Link href="#">Clases</Nav.Link>
-            <Nav.Link href="#">Estudio</Nav.Link>
-            <Nav.Link href="#">Sobre Fina</Nav.Link>
-            <Nav.Link href="#">Contacto</Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav" className="nav-collapse">
+            <Nav className="mx-auto Nav nav-links">
+              <Nav.Link href="#">Clases</Nav.Link>
+              <Nav.Link href="#">Estudio</Nav.Link>
+              <Nav.Link href="#">Sobre Fina</Nav.Link>
+              <Nav.Link href="#">Contacto</Nav.Link>
+            </Nav>
 
-          <NavDropdown
-            title="Idioma"
-            id="basic-nav-dropdown"
-            className="nav-dropdown navbar-right"
-          >
-            <NavDropdown.Item href="#action/3.1">Castellano</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">English</NavDropdown.Item>
-          </NavDropdown>
+            <NavDropdown
+              title="Idioma"
+              id="basic-nav-dropdown"
+              className="nav-dropdown navbar-right"
+            >
+              <NavDropdown.Item href="#action/3.1">Castellano</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">English</NavDropdown.Item>
+            </NavDropdown>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
