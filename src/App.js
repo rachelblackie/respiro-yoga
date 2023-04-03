5ad3e5056babe75434a36d967c978b078af96bfe
rachelblackie <rachelannblackie@gmail.com>
2023-04-03 10:48:52 +0200

    css changes, loading screen

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Menu from "./Menu";
 import Hero from "./Hero";
@@ -7,16 +8,33 @@ import Fina from "./Fina";
 import Contacto from "./Contacto";
 
 function App() {
-  return (
-    <div className="App">
-      <Menu />
-      <Hero />
-      <Clases />
-      <Estudio />
-      <Fina />
-      <Contacto />
-    </div>
-  );
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    setReady(true);
+  }, []);
+
+  if (ready) {
+    return (
+      <div className="App">
+        <Menu />
+        <Hero />
+        <Clases />
+        <Estudio />
+        <Fina />
+        <Contacto />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1 className="text-center mt-5">Loading...</h1>
+        <h2 className="text-center">
+          <i class="fa-sharp fa-solid fa-fire-flame-curved"></i>
+        </h2>
+      </div>
+    );
+  }
 }
 
 export default App;
